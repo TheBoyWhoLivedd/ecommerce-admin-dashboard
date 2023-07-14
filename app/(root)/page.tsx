359f2b4a3af7024c3c7df88824131267f503c295
TheBoyWhoLivedd <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-07-14 17:35:52 +0300

    Added Store Modal and Form component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
       <p>Protected Route</p>
