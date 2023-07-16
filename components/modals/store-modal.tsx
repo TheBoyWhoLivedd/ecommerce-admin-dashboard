46f953084bfcf0697316eb4608eb5342ed07fabb
TheBoyWhoLivedd <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2023-07-16 22:57:40 +0300

    Added Prisma

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -66,7 +66,7 @@ export const StoreModal = () => {
                           {...field}
                         />
                       </FormControl>
-                      <FormMessage />
+                      <FormMessage  />
                     </FormItem>
                   )}
                 />
